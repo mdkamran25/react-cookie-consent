f378948a43a005f094330966b1ffbd19f36e2d31
Md Kamran <mdkamran12310@gmail.com>
2023-10-26 19:42:23 +0530

    gtag

--- Desktop/React-Cookie-Consent/cookie-consent/src/App.js
+++ Desktop/React-Cookie-Consent/cookie-consent/src/App.js
@@ -4,29 +4,44 @@ import { Route } from "react-router-dom";
 import Home from "./Pages/home";
 import About from "./Pages/about";
 import Contact from "./Pages/contact";
-import CookieConsent, {getCookieConsentValue} from "react-cookie-consent";
-import { useEffect, useState } from "react";
-import Cookies from 'js-cookie';
-import ReactGA from 'react-ga4'
+import CookieConsent from "react-cookie-consent";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import ReactGA from "react-ga4";
+import { v4 as uuidv4 } from "uuid";
 
-const TRACKING_ID = "G-5HP87K5S6F"
-// ReactGA.initialize(TRACKING_ID)
-ReactGA.initialize([ { trackingId: TRACKING_ID, gaOptions: { debug_mode: true, }, gtagOptions: { debug_mode: true, }, }, ]);
+
+const TRACKING_ID = "G-5HP87K5S6F";
+ReactGA.initialize([
+  {
+    trackingId: TRACKING_ID,
+    gaOptions: { debug_mode: true },
+    gtagOptions: { debug_mode: true },
+  },
+]);
 function App() {
-  // const [hasConsentValue, setHasConsentValue] = useState(false)
-
+  const gtm_id = process.env.GTM_ID
+  console.log(gtm_id, "gtm_id")
+  useEffect(() => {
+    if (!Cookies.get("utm")) {
+      const id = uuidv4().substring(0, 4);
+      Cookies.set("utm", id);
+    }
+  }, []);
   const loadGoogleAnalytics = () => {
-    console.log("Google Analytics Activity")
-    Cookies.set("Cookies-set", "Hello from Home Page", {expires: 7})
-  }
-
+    console.log("Google Analytics Activity");
+    Cookies.set("Cookies-set", "Hello from Home Page", { expires: 7 });
+  };
 
   ReactGA.send({
     hitType: "pageview",
-    page: window.location.pathname
+    page: window.location.pathname,
   });
+
+  
   return (
     <div className="App">
+      
       <div className="navbar">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
@@ -39,29 +54,32 @@ function App() {
         <Route element={<Contact />} path="/contact" />
       </Routes>
       <CookieConsent
-      enableDeclineButton
-      onAccept={loadGoogleAnalytics}
-      buttonText="Accept Cookies"
-      declineButtonText="Decline"
-      buttonStyle={{
-        color: "white",
-        background: "Green",
-        fontSize: "1rem",
-        border: "1px solid black",
-        borderRadius: "5px",
-        width: "9rem"
-      }}
-      declineButtonStyle={{
-        color: "white",
-        background: "red",
-        fontSize: "1rem",
-        border: "1px solid black",
-        borderRadius: "5px",
-        width: "8rem"
-      }}
+        enableDeclineButton
+        onAccept={loadGoogleAnalytics}
+        buttonText="Accept Cookies"
+        declineButtonText="Decline"
+        buttonStyle={{
+          color: "white",
+          background: "Green",
+          fontSize: "1rem",
+          border: "1px solid black",
+          borderRadius: "5px",
+          width: "9rem",
+        }}
+        declineButtonStyle={{
+          color: "white",
+          background: "red",
+          fontSize: "1rem",
+          border: "1px solid black",
+          borderRadius: "5px",
+          width: "8rem",
+        }}
         style={{
           font: "1.5rem",
-          textAlign: "left", background: "black", paddingTop: "0.5rem", paddingBottom: "0.5rem"
+          textAlign: "left",
+          background: "black",
+          paddingTop: "0.5rem",
+          paddingBottom: "0.5rem",
         }}
         expires={1}
       >
--- Desktop/React-Cookie-Consent/cookie-consent/src/Pages/contact.jsx
+++ Desktop/React-Cookie-Consent/cookie-consent/src/Pages/contact.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import ReactGA from "react-ga4"
+import React, { useEffect, useState } from "react";
+import ReactGA from "react-ga4";
+import Cookies from "js-cookie";
 const Contact = () => {
-    ReactGA.send({
-        hitType: "pageview",
-        page: window.location.pathname,
-        title: "/contact"
-      });
+  const [utm_id, setUtm_id] = useState();
+
+  useEffect(() => {
+    if (Cookies.get("utm")) {
+      setUtm_id(Cookies.get("utm"));
+    }
+  }, []);
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+    title: "/contact",
+  });
+  
+  const [decrement, setdecerement] = useState(0);
+  
+  const clickHandler = () => {
+    setdecerement((e) => e - 1);
+    const eventTime = new Date().toISOString();
+
+    
+    ReactGA.event({
+      category: "Button Click",
+      action: "Decrement Click",
+      label: utm_id,
+      value: "mdkamran7255@gmail.com",
+      eventTime: eventTime,
+    });
+  };
   return (
     <div>
       <h1>Contact</h1>
+      <button onClick={clickHandler}>Decermernt: {decrement}</button>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
